7d18107c55e6c4aabd744cb9c823d68c8542a809
evln77 <queersciencelab@proton.me>
2026-08-25 22:16:38 -0700

    Update scraping.R

--- data retrieval/scraping.R
+++ data retrieval/scraping.R
@@ -8,28 +8,31 @@ segm_summaries_css2 <- html_text(summaries_css)
 # extraction 
 library(tidyverse)
 abstract <- gsub(".*\\Journal Abstract ", "", segm_summaries_css)
-segm_table <- as.data.frame(abstract)
+segm_compendium <- as.data.frame(abstract)
 segm_summaries_css <- gsub("\n  \n    JOURNAL ABSTRACT\n .*","",segm_summaries_css)
 segm_summaries_css <- gsub("^\n   ","",segm_summaries_css)
 doi_link <- gsub(".*\\https://","",segm_summaries_css)
 doi_link <- as.data.frame(doi_link)
-segm_table <- cbind(segm_table,doi_link)
+segm_compendium <- cbind(segm_compendium,doi_link)
 authors <- gsub("\n.*","", segm_summaries_css)
 authors <- as.data.frame(authors)
-segm_table <- cbind(authors,segm_table)
+segm_compendium <- cbind(authors,segm_compendium)
 segm_summaries_css <- gsub(",\n.*","",segm_summaries_css)
 journal <- gsub(".*\\.\n    ","",segm_summaries_css)
 journal <- as.data.frame(journal)
-segm_table <- cbind(journal,segm_table)
+segm_compendium <- cbind(journal,segm_compendium)
 segm_summaries_css <- gsub(".\n    .*","",segm_summaries_css)
 title <- gsub(".*\\)\n  ","",segm_summaries_css)
 title <- as.data.frame(title)
-segm_table <- cbind(title,segm_table)
+segm_compendium <- cbind(title,segm_compendium)
 segm_summaries_css <- gsub(")\n  .*","",segm_summaries_css)
 year <- gsub(".*\\(","",segm_summaries_css)
 year <- as.data.frame(year)
-segm_table <- cbind(segm_table,year)
-segm_table$year <- as.numeric(unlist(segm_table$year))
-segm_table %>%
+segm_compendium <- cbind(segm_compendium,year)
+segm_compendium$year <- as.numeric(unlist(segm_compendium$year))
+segm_compendium %>%
   distinct(.keep_all = TRUE) ->
-  segm_table
+  segm_compendium
+
+# save
+write.csv(segm_compendoum, "segm_compendium.csv", row.names = FALSE)
