349db980dbde355ccf7d76f31ddd8b0cf4a7a269
evln77 <queersciencelab@proton.me>
2026-08-25 22:18:19 -0700

    Update journal_counts.R

--- data retrieval/journal_counts.R
+++ data retrieval/journal_counts.R
@@ -1,13 +1,15 @@
+# load compendium
+segm_compendium <- read_csv("segm_compendium.csv")
+
 # create journal counts
-segm_table %>% count(journal) %>% arrange(desc(n)) %>% filter(journal != "NA") -> journal_counts
+segm_compendium %>% count(journal) %>% arrange(desc(n)) %>% filter(journal != "NA") -> journals
 
 ## load APCs and publishers
 journal_info <- read_csv("journals_info.csv")
 
 # add apc and publisher info
-merge(journal_counts, journal_info[, c("journal", "apc", "parent")], by="journal") -> journal_counts
-journal_counts$profit <- journal_counts$n*journal_counts$apc
-write.csv(journal_counts, "segm_journals.csv", row.names = FALSE)
+merge(journals, journal_info[, c("journal", "apc", "parent")], by="journal") -> journal_counts
+journals$profit <- journals$n*journals$apc
+write.csv(journals, "segm_journals.csv", row.names = FALSE)
 
-aggregate(journal_counts$profit, by=list(parent=journal_counts$parent), FUN=sum) -> pub_profits
-pub_profits <- pub_profits[order(-pub_profits$x),]
+
