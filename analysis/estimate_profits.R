8ccfe8ca0007e45125e08570ed315d3b0d7eb2f7
evln77 <queersciencelab@proton.me>
2026-08-26 22:53:02 -0700

    Update estimate_profits.R

--- analysis/estimate_profits.R
+++ analysis/estimate_profits.R
@@ -1,3 +1,7 @@
+# load packages
+library(tidyverse)
+
+# calculate profits
 aggregate(journal_counts$profit, by=list(parent=journal_counts$parent), FUN=sum) -> pub_profits
 pub_profits <- pub_profits[order(-pub_profits$x),]
 sum(journal_counts$profit, na.rm=TRUE)
